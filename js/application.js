bfe140ab07d96e8d3dccc906e02a96d244be2935
Eric Tan <20925931@student.uwa.edu.au>
2013-12-17 08:32:11 +0800

    My League Experience section complete, images uploaded and code clean up

--- js/application.js
+++ js/application.js
@@ -8,3 +8,15 @@ $(function () {
 	
 	$('.nextdiv').localScroll(5000);
 });
+
+function imagePopUp() {
+	document.getElementById('fade').style.display='block';
+	document.getElementById('light').style.display='block';
+	$('body').addClass('stop-scrolling')
+}
+
+function closePopUp() {
+	document.getElementById('fade').style.display='none';
+	document.getElementById('light').style.display='none';
+	$('body').removeClass('stop-scrolling')
+}
