460c50de83e52c809574af5c357dfd0ee21c97ab
Eric Tan <20925931@student.uwa.edu.au>
2013-12-20 03:56:04 +0800

    Resize tab fixed, centered images more fluid

--- js/application.js
+++ js/application.js
@@ -40,44 +40,26 @@ $(function () {
 		responsive: true
 	});
 	
-	recalcWindowSize();
+	//Set the pop up image to a maximum height - avoid covering the screen
+	$('.bigimage').css("max-height", $(window).height() * 0.75);
 	
 	//Set the alert for when a user tries to resize their window!
 	//Please don't harm the kittens :(
 	$(window).resize(function() {
 		$('#resizetab').css("opacity", "1");
-		$('#resizetab').css("height", "30px");
-		recalcWindowSize();
+		$('#resizetab').css("left", "50%");
+		
+		//Recalculate pop up image max height - avoid covering the screen
+		$('.bigimage').css("max-height", $(window).height() * 0.75);
 	});
 });
 
 function hideResize() {
 	$('#resizetab').css("opacity", "0");
-	$('#resizetab').css("height", "0");
+	$('#resizetab').css("left", "200%");
 }
 
-function recalcWindowSize() {
-	//set the pop up image to a maximum height - avoid covering the screen
-	$('.bigimage').css("max-height", $(window).height() * 0.75);
-	
-	//Set negative margin-left and margin-right's for the image pop ups - relative to window size
-	$('#PUIwins').css("margin-left", $('#PUIwins').width() * -0.5);
-	$('#PUIwins').css("margin-top", $('#PUIwins').height() * -0.5);
-	$('#PUIgolds1').css("margin-left", $('#PUIgolds1').width() * -0.5);
-	$('#PUIgolds1').css("margin-top", $('#PUIgolds1').height() * -0.5);
-	$('#PUIplat').css("margin-left", $('#PUIplat').width() * -0.5);
-	$('#PUIplat').css("margin-top", $('#PUIplat').height() * -0.5);
-	$('#PUIsinged').css("margin-left", $('#PUIsinged').width() * -0.5);
-	$('#PUIsinged').css("margin-top", $('#PUIsinged').height() * -0.5);
-	$('#PUIdiamond').css("margin-left", $('#PUIdiamond').width() * -0.5);
-	$('#PUIdiamond').css("margin-top", $('#PUIdiamond').height() * -0.5);
-	$('#PUIbracket').css("margin-left", $('#PUIbracket').width() * -0.5);
-	$('#PUIbracket').css("margin-top", $('#PUIbracket').height() * -0.5);
-	$('#PUIshirt').css("margin-left", $('#PUIshirt').width() * -0.5);
-	$('#PUIshirt').css("margin-top", $('#PUIshirt').height() * -0.5);
-}
-
-/* Self-produced jQuery and CSS for image pop ups. NO PLUG IN USED! */
+/* Self-produced jQuery and CSS for image pop ups. NO PLUG-IN USED! */
 function showPopUp() {
 	$('#fade').css("opacity", "0.8");
 	$('#fade').css("height", "100%");
@@ -93,43 +75,57 @@ function closePopUp() {
 
 function puiWins() {
 	showPopUp();
+	$('#PUIwins').css("margin-left", $('#PUIwins').width() * -0.5);
+	$('#PUIwins').css("margin-top", $('#PUIwins').height() * -0.5);
 	$('#PUIwins').css("display", "block");
 }
 
 function puiGoldS1() {
 	showPopUp();
+	$('#PUIgolds1').css("margin-left", $('#PUIgolds1').width() * -0.5);
+	$('#PUIgolds1').css("margin-top", $('#PUIgolds1').height() * -0.5);
 	$('#PUIgolds1').css("display", "block");
 }
 
 function puiPlat() {
 	showPopUp();
+	$('#PUIplat').css("margin-left", $('#PUIplat').width() * -0.5);
+	$('#PUIplat').css("margin-top", $('#PUIplat').height() * -0.5);
 	$('#PUIplat').css("display", "block");
 }
 
 function puiSinged() {
 	showPopUp();
+	$('#PUIsinged').css("margin-left", $('#PUIsinged').width() * -0.5);
+	$('#PUIsinged').css("margin-top", $('#PUIsinged').height() * -0.5);
 	$('#PUIsinged').css("display", "block");
 }
 
 function puiDiamond() {
 	showPopUp();
+	$('#PUIdiamond').css("margin-left", $('#PUIdiamond').width() * -0.5);
+	$('#PUIdiamond').css("margin-top", $('#PUIdiamond').height() * -0.5);
 	$('#PUIdiamond').css("display", "block");
 }
 
 function puiBracket() {
 	showPopUp();
+	$('#PUIbracket').css("margin-left", $('#PUIbracket').width() * -0.5);
+	$('#PUIbracket').css("margin-top", $('#PUIbracket').height() * -0.5);
 	$('#PUIbracket').css("display", "block");
 }
 
 function puiShirt() {
 	showPopUp();
+	$('#PUIshirt').css("margin-left", $('#PUIshirt').width() * -0.5);
+	$('#PUIshirt').css("margin-top", $('#PUIshirt').height() * -0.5);
 	$('#PUIshirt').css("display", "block");
 }
 
 /* Code to disable scrolling (useful when having CSS image-pop ups
-    - WITHOUT removing the scrollbar from the visible window (ie by
-	setting overflow:hidden on body -
-   Taken from http://jsbin.com/disable-scrolling/1                 */
+		- WITHOUT removing the scrollbar from the visible window (ie by
+		setting 'overflow: hidden' on body
+   Taken from http://jsbin.com/disable-scrolling/1 */
 var keys = [37, 38, 39, 40];
 
 function preventDefault(e) {
